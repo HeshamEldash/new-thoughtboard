0d64c74c0de8f363abe04802330f1fc1900d92eb
hesha <example@gmail.com>
2022-07-24 09:35:39 +0100

    CRRECTED

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
-ALLOWED_HOSTS = "new-thought-board.herokuapp.com/"
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
+# ALLOWED_HOSTS = "new-thought-board.herokuapp.com/"
 
 # Application definition
 
